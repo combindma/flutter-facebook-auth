a0bb9c21a0bbc073b05f2fa0f9fe73094eb8b042
Darwin Morocho <darwin.morocho@icloud.com>
2026-07-01 06:54:18 -0500

    chore: update Android build configuration, upgrade Gradle, and remove legacy Kotlin plugin/plugin file

--- flutter_facebook_auth/example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ flutter_facebook_auth/example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/darwin/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/darwin/Documents/flutter/flutter-facebook-auth/facebook_auth/example"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=/Users/darwin/Documents/flutter/flutter-facebook-auth/facebook_auth/example/macos/Flutter/ephemeral/Packages/.packages/FlutterFramework"
+export "FLUTTER_APPLICATION_PATH=/Users/darwin/Documents/flutter/flutter-facebook-auth/flutter_facebook_auth/example"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=/Users/darwin/Documents/flutter/flutter-facebook-auth/flutter_facebook_auth/example/macos/Flutter/ephemeral/Packages/.packages/FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
